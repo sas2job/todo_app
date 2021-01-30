fb88080fb5f79556ca4476be1a5416d21d2f43b8
Alexander Smirnov <sas2job@gmail.com>
2021-01-30 11:03:51 +0300

    Add stimulus to todo_item

---
+++ app/reflexes/todo_item_reflex.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class TodoItemReflex < ApplicationReflex
+
+  def mark_incomplete
+    todo_item = TodoItem.find(element.dataset.id)
+    todo_item.update(completed: false, completed_at: nil)    
+  end
+
+  def mark_complete
+    todo_item = TodoItem.find(element.dataset.id)
+    todo_item.update(completed: true, completed_at: Time.now)
+  end
+end
